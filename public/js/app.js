969a91a2f5b9f0b4b64571a490e573de4a53d2a6
smctigue <stephenmctigue@gmail.com>
2015-08-26 14:15:26 -0700

    dash-three

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
 function getFoods() {
 	$.get("/foods", function(res) {
-		console.log(res);
+		// console.log(res);
+		// template response of food items
+		  var foods = JSON.parse(res);
+		// generate a template mold
+		var foodTemplate = _.template($("#food-template").html());
+		// clears existing items
+		$("#food-ul").empty();
+		// iterates through foods
+		foods.forEach(function (food) {
+			// return foodTemplate(food);
+			var foodHTML = foodTemplate(food);
+			$("#food-ul").append(foodHTML);
+		});
 	});
 };
